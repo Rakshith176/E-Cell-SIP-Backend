7dcbb42c1b8f8d00d695044d24110529caa84ce6
Rakshith176 <rakshithh176@gmail.com>
2020-09-15 22:39:25 +0530

    final

--- e_cell/__init__.py
+++ e_cell/__init__.py
@@ -5,6 +5,7 @@ app=Flask(__name__)
 
 
 app.config['SECRET_KEY']='e8556002376cf2e3d081aab7db746234'
+#using splite database(can be changed to any database if required)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///startup.db'
 db = SQLAlchemy(app)
 
--- e_cell/routes.py
+++ e_cell/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template, url_for, redirect, request,send_file
+from flask import Flask,render_template, url_for, redirect, request,send_file,flash
 from e_cell import app,db
 from e_cell.forms import Startup_Registration
 from e_cell.models import StartUp
@@ -13,9 +13,9 @@ def register():
 		startup= StartUp(startup_name=form.startup_name.data,poc_name=form.poc_name.data,poc_email=form.poc_email.data,poc_phone_no=form.poc_phone_no.data,profile_doc=file.read(),incentive=form.incentive.data,duration=form.duration.data,website=form.website.data)
 		db.session.add(startup)
 		db.session.commit()
+		flash(f'Congrats your startup {form.startup_name.data} is added.. You can see it in the list below','success')
 		return redirect(url_for('apply'))
 	return render_template('startup_register.html',form=form,title='E-Cell|SIP Startups')
-
 
 
 @app.route("/apply",methods=['GET','POST'])
@@ -24,6 +24,7 @@ def apply():
 	return render_template('startup_apply.html',title='E-Cell|SIP Students',startup_list=startup_list)
 
 @app.route("/download<int:id>",methods=['GET'])
+#a function to download the profile of the startup
 def download(id):
 	if id:
 		file_data = StartUp.query.filter_by(id=id).first() #this filters the required file to download by taking the id arugument from the download button from the startup details
